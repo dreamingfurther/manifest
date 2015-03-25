3d3fa5ef0bb68dd70ccb4febd20787212f853623
David Tengdin <dreamingfurther@gmail.com>
2015-03-25 09:17:04 -0400

    Add some crazy nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   root 'planes#index'
 
   resources :planes, only: [:index, :new]
+
+  resources :blog, only: [:show] do
+    resources :comment, only: [:show]
+  end
+
+  resources :world, only: [:show] do
+    resources :country, only: [:show] do
+      resources :state, only: [:show] do
+        resources :county, only: [:show] do
+          resources :city, only: [:show]
+        end
+      end
+    end
+  end
 end
